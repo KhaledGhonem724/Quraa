6eb637eb875653110f6bb69e682e565645e40013
KhaledGhonem724 <KhaledGhonem724@gmail.com>
2023-10-15 15:17:32 +0300

    few edited

--- dist/nav.js
+++ dist/nav.js
@@ -9,6 +9,19 @@ function showOptions() {
     }
     optionsHidden = !optionsHidden;
 }
+function showSlideOver() {
+    document.getElementById("slide-over-opacity-item background-backdrop").className = "fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity ease-in-out duration-500 opacity-75 ";
+    document.getElementById("slide-over-opacity-item close-button").className = "absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4 ease-in-out duration-500 opacity-75 ";
+    document.getElementById("slide-over-translate-item").className = "pointer-events-auto relative w-screen max-w-md transform transition ease-in-out duration-500 sm:duration-700 translate-x-0";
+    document.getElementById("the-slide").className = "relative z-10";
+}
+function hideSlideOver() {
+    document.getElementById("slide-over-opacity-item background-backdrop").className = "fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity ease-in-out duration-500 opacity-0 ";
+    document.getElementById("slide-over-opacity-item close-button").className = "absolute left-0 top-0 -ml-8 flex pr-2 pt-4 sm:-ml-10 sm:pr-4 ease-in-out duration-500 opacity-0 ";
+    document.getElementById("slide-over-translate-item").className = "pointer-events-auto relative w-screen max-w-md transform transition ease-in-out duration-500 sm:duration-700 translate-x-full";
+    document.getElementById("the-slide").className = "relative -z-10";
+}
+
 let pagesIDs = ["homepage", "contactus", "aboutus"];
 function color(id) {
     document.getElementById(id).style.color = 'salmon';
